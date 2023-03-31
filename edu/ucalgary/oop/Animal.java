363978d9bc967ae35951cbe5e444ed7ccf555243
ishahaider <isha.haider213@gmail.com>
2023-03-31 16:32:23 -0600

    reuploaded files for Java8

---
+++ edu/ucalgary/oop/Animal.java
@@ -0,0 +1,28 @@
+package edu.ucalgary.oop;
+
+public class Animal {
+    private int animalID;
+    private String animalName;
+    private String animalSpecies;
+    private String mostActive;
+
+    public Animal (int id, String name, String species ){
+        this.animalID = id;
+        this.animalName = name;
+        this.animalSpecies = species;
+
+        if (this.animalSpecies.equals(Species.COYOTE.toString()) || this.animalSpecies.equals(Species.PORCUPINE.toString())) 
+            this.mostActive = "Crepuscular";
+        if (this.animalSpecies.equals(Species.FOX.toString()) || this.animalSpecies.equals(Species.RACCOON.toString())) 
+            this.mostActive = "Nocturnal";
+        if (this.animalSpecies.equals(Species.BEAVER.toString())) 
+            this.mostActive = "Diurnal";
+    }
+    public void setID(int id) { this.animalID = id; }
+    public int getID() { return this.animalID; }
+    public void setNickname(String name) { this.animalName = name; }
+    public String getNickname() { return this.animalName; }
+    public void setSpecies(String species) { this.animalSpecies = species; }
+    public String getSpecies() { return this.animalSpecies; }
+    public String getMostActive() { return this.mostActive; }
+}
---
+++ edu/ucalgary/oop/Treatment.java
@@ -0,0 +1,29 @@
+package edu.ucalgary.oop;
+
+public class Treatment {
+    private int treatmentID;
+    private int animalID;
+    private int taskID;
+    private int startHour;
+
+    public Treatment (int treatmentID, int animalID, int taskID, int startHour){
+        this.treatmentID = treatmentID;
+        this.animalID = animalID;
+        this.taskID = taskID;
+        this.startHour = startHour;
+    }
+    public Treatment (int treatmentID, int animalID, int taskID){
+        this.treatmentID = treatmentID;
+        this.animalID = animalID;
+        this.taskID = taskID;
+        this.startHour = 24;
+    }
+    public void setTreatementID(int id) { this.treatmentID = id; }
+    public int getTreatementID() { return this.treatmentID; }
+    public void setAnimalID(int id) { this.animalID = id; }
+    public int getAnimalID() { return this.animalID; }
+    public void setTaskID(int id) { this.taskID = id; }
+    public int getTaskID() { return this.taskID; }
+    public void setStartHour(int startHour) { this.startHour = startHour; }
+    public int getStartHour() { return this.startHour; } 
+}
