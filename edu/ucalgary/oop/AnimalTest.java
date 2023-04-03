6d0077a1a5383be757d2a181c85f5b9bc38488e1
Saba <sabayara0@gmail.com>
2023-04-03 10:35:09 -0600

    just a testing commit not the actual test file

---
+++ edu/ucalgary/oop/AnimalTest.java
@@ -0,0 +1,17 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.regex.*;
+public class AnimalTest {
+
+    @Test 
+    public void testAnimalConstructor() {
+        // Values used for all tests involving Animal class
+        string expectedAnimalID = "1";
+        string expectedAnimalName = "Emma";
+        string expectedAnimalSpecies = "fox"; 
+
+
+    }
+}
