e7769249084354d1e2e16dfcfde8695af23195cf
Saba <sabayara0@gmail.com>
2023-04-04 17:03:19 -0600

    final draft of  animal tests

--- edu/ucalgary/oop/LoadDataTest.java
+++ edu/ucalgary/oop/LoadDataTest.java
@@ -15,7 +15,7 @@ public class LoadDataTest{
     Animal sharedTestObj = new Animal(expectedAnimalID, expectedAnimalNickname,
     expectedAnimalSpecies);
 
-    @Test
+    @Test  // will the animal constructor create an Animal object without an exception?
     public void testAnimalGetters() {
         int actualAnimalID = sharedTestObj.getID();
         String actualAnimalNickname = sharedTestObj.getNickname();
@@ -24,5 +24,57 @@ public class LoadDataTest{
         assertEquals("Constructor or getter gave wrong value for Animal ID", expectedAnimalID, actualAnimalID);
         assertEquals("Constructor or getter gave wrong value for Animal Nickname", expectedAnimalNickname, actualAnimalNickname);
         assertEquals("Constructor or getter gave wrong value for Animal spieces", actualAnimalSpecies, actualAnimalSpecies);
+    }
+    @Test // has mostActive set attribute to different values depending on the species?
+    public void testConstructorCrepuscular() {
+        Animal animal1 = new Animal(2, "Olivia", Species.COYOTE.toString());
+        assertEquals("Crepuscular", animal1.getMostActive());
+
+        Animal animal2 = new Animal(3, "Emily", Species.PORCUPINE.toString());
+        assertEquals("Crepuscular", animal2.getMostActive());
+    }
+    @Test // has mostActive set attribute to different values depending on the species?
+    public void testConstructorNocturnal() {
+        Animal animal1 = new Animal(4, "Sly", Species.FOX.toString());
+        assertEquals("Nocturnal", animal1.getMostActive());
+
+        Animal animal2 = new Animal(5, "Rocky", Species.RACCOON.toString());
+        assertEquals("Nocturnal", animal2.getMostActive());
+    }
+
+    @Test // has mostActive set attribute to different values depending on the species?
+    public void testConstructorDiurnal() {
+        Animal animal = new Animal(6, "Bevy", Species.BEAVER.toString());
+        assertEquals("Diurnal", animal.getMostActive());
+    }
+
+    @Test(expected = NullPointerException.class) // tests the constructor with invalid data that should throw a NullPointerException
+    public void testConstructorInvalidSpecies() {
+        new Animal(7, "Socks", null);
+    }
+
+    @Test // tests the setters and getters lol 
+    public void testSettersAndGetters() {
+        Animal animal = new Animal(8, "Spot", Species.FOX.toString());
+
+        animal.setID(9);
+        assertEquals(9, animal.getID());
+
+        animal.setNickname("Patches");
+        assertEquals("Patches", animal.getNickname());
+
+        animal.setSpecies(Species.RACCOON.toString());
+        assertEquals("raccoon", animal.getSpecies());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidSpecies() {
+        Animal invalidAnimal = new Animal(1, "Random Animal", "Random species");
+    }
+
+    @Test
+    public void testValidSpecies() {
+        Animal validAnimal = new Animal(2, "Valid Animal", Species.COYOTE.toString());
+        assertNotNull(validAnimal);
     }
 }
