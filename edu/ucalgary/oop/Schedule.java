ab5174c0f5260d161cd6de25c6f0fe243993a2d6
ishahaider <isha.haider213@gmail.com>
2023-04-03 21:25:13 -0600

    maxWindows 1 to 5

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -2,14 +2,16 @@ package edu.ucalgary.oop;
 
 public class Schedule {
     private String tasksList;
-    private int quantity;
+    private int startTime;
+    private int quantity = 1;
     private int timeSpent;
-    private int timeRemaining;
-    private boolean backupRequired;
+    private int timeRemaining = 60;
+    private boolean backupRequired = false;
 
     public Schedule(){}
-    public Schedule(String tasksList, int quantity, int timeSpent, int timeRemaining, boolean backupRequired){
+    public Schedule(String tasksList, int startTime, int quantity, int timeSpent, int timeRemaining, boolean backupRequired){
         this.tasksList = tasksList;
+        this.startTime = startTime;
         this.quantity = quantity;
         this.timeSpent = timeSpent;
         this.timeRemaining = timeRemaining;
@@ -18,6 +20,8 @@ public class Schedule {
     
     public void setTasksList(String tasksList) { this.tasksList = tasksList; }
     public String getTasksList() { return this.tasksList; }
+    public void setStartTime(int startTime) { this.startTime = startTime; }
+    public int getStartTime() { return this.startTime; }
     public void setQuantity(int quantity) { this.quantity = quantity; }
     public int getQuantity() { return this.quantity; }
     public void setTimeSpent(int timeSpent) { this.timeSpent = timeSpent; }
