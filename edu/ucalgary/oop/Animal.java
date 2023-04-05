e894251b17edeb0019219a910e8603f653721d93
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2023-04-04 20:33:38 -0600

    Animal.java documentation

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -1,28 +1,58 @@
+/** 
+@author ENSF380 Group 20
+* Animal is a java class representing one row of data from the Animal table of the EWR database. 
+@version    1.2
+@since 1.0
+*/
 package edu.ucalgary.oop;
 
 public class Animal {
     private int animalID;
     private String animalName;
     private String animalSpecies;
-    private String mostActive;
+    private final String MOSTACTIVE;
 
-    public Animal (int id, String name, String species ){
+    /** Constructor
+    * Initializes the data members of the Animal class
+    * Sets the MOSTACTIVE data member using the Species enumeration:
+    ** Species: coyote or porcupine --> mostActive = Crepuscular
+    ** Species: fox or raccon --> mostActive = Nocturnal
+    ** Species: beaver--> mostActive = Diurnal
+    * @param  id  an integer value of the animal ID
+    * @param  name a string of the animal nickname
+    * @param  species a string of the animal species
+    * @throws SpeciesNotFoundException
+    * @return  
+    */
+    public Animal (int id, String name, String species) throws SpeciesNotFoundException {
         this.animalID = id;
         this.animalName = name;
         this.animalSpecies = species;
 
         if (this.animalSpecies.equals(Species.COYOTE.toString()) || this.animalSpecies.equals(Species.PORCUPINE.toString())) 
-            this.mostActive = "Crepuscular";
-        if (this.animalSpecies.equals(Species.FOX.toString()) || this.animalSpecies.equals(Species.RACCOON.toString())) 
-            this.mostActive = "Nocturnal";
-        if (this.animalSpecies.equals(Species.BEAVER.toString())) 
-            this.mostActive = "Diurnal";
+            MOSTACTIVE = "Crepuscular";
+        else if (this.animalSpecies.equals(Species.FOX.toString()) || this.animalSpecies.equals(Species.RACCOON.toString())) 
+            MOSTACTIVE = "Nocturnal";
+        else if (this.animalSpecies.equals(Species.BEAVER.toString())) 
+            MOSTACTIVE = "Diurnal";
+        else { 
+            throw new SpeciesNotFoundException("the animal " + name + " is not of a permitted species: " 
+            + species + "please only enter animals of the following species: coyote, porcupine, fox, raccoon, or beaver"); }
     }
+
+    /** Setters 
+    * setter methods assigning the parameter value to the stored object
+    * @return   void
+    */
     public void setID(int id) { this.animalID = id; }
-    public int getID() { return this.animalID; }
     public void setNickname(String name) { this.animalName = name; }
-    public String getNickname() { return this.animalName; }
     public void setSpecies(String species) { this.animalSpecies = species; }
+
+    /** Getters
+    * getter methods returning the stored object requested
+    */
+    public int getID() { return this.animalID; }
+    public String getNickname() { return this.animalName; }
     public String getSpecies() { return this.animalSpecies; }
-    public String getMostActive() { return this.mostActive; }
+    public String getMostActive() { return this.MOSTACTIVE; }
 }
